66bbd502cb0f23235c127843cf593f5b32035e91
Jehy <Jehy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-04 12:55:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rss-atom-avatar/trunk@142734 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rss-atom-avatar.php
+++ rss-atom-avatar.php
@@ -11,7 +11,7 @@ Version: 1.1
 Author: Jehy
 Author URI: http://jehy.ru/index.en.html
 Min WP Version: 2.5
-Max WP Version: 2.8.2
+Max WP Version: 2.8.3
 */
 
 /*  Copyright 2008  Jehy  (email : jehy@valar.ru)
